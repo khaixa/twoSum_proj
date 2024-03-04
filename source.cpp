02326badbb123ed1b4cd0e849d94b6a970b3eab3
khaixa <n8tennyson@gmail.com>
2024-03-03 20:35:10 -0500

    030324

--- source.cpp
+++ source.cpp
@@ -7,6 +7,22 @@ std::vector<int> readNumbersFromFile(const std::string& filename){
     std::vector<int> numbers;
     std::ifstream file(filename);
     int num;
+    while (file >> num){
+        numbers.push_back(num);
+    }
+    file.close();
+    return numbers;
+}
+
+long long performSummation(const std::vector<int>& numbers){
+    long long sum = 0;
+    int n = numbers.size();
+    for (int i = 0; i < n; ++i){
+        for (int j = i + 1; j < n; ++j){
+            sum += numbers[i] + numbers[j];
+        }
+    }
+    return sum;
 }
 
 int main(){
