02aaf0326299f1be35d8c30c364541d1815acea9
khaixa <n8tennyson@gmail.com>
2024-03-03 20:30:47 -0500

    first commit

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+
+std::vector<int> readNumbersFromFile(const std::string& filename){
+    std::vector<int> numbers;
+    std::ifstream file(filename);
+    int num;
+}
+
+int main(){
+
+
+
+
+    return 0;
+}
